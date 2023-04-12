d7b76f49040984ef2160445854deab93687796d0
samoma451 <samoma451@gmail.com>
2023-04-12 16:27:30 +0100

    Connected app to firebase storage. Implemented Camera and Gallery to select image to be posted. Implemented upload of image to firebase storage. Note that the "caption" editText added to the NewPostFragment is a nonfunctional placeholder.

--- app/src/main/java/com/example/cs4084project/NewPostFragment.java
+++ app/src/main/java/com/example/cs4084project/NewPostFragment.java
@@ -2,7 +2,41 @@ package com.example.cs4084project;
 
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.model.Dash;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.util.UUID;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,6 +57,19 @@ public class NewPostFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+
+    private FirebaseStorage firebaseStorage;
+    private StorageReference storageReference;
+
+    ImageView imageView;
+    Uri imageUri;
+    Button galleryBtn;
+    Button cameraBtn;
+
+    TextView captionTxt;
+
+    Button uploadBtn;
 
     public NewPostFragment() {
         // Required empty public constructor
@@ -53,12 +100,138 @@ public class NewPostFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+
+        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_DENIED
+        ) {
+            String[] permission = {
+                    android.Manifest.permission.CAMERA
+            };
+            requestPermissions(permission, 100);
+        }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_new_post, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
+        imageView = getView().findViewById(R.id.postImage);
+        galleryBtn =  getView().findViewById(R.id.gallery);
+        cameraBtn =  getView().findViewById(R.id.camera);
+
+        uploadBtn =  getView().findViewById(R.id.uploadPost);
+
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+
+        galleryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent =
+                        new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                galleryActivityResultLauncher.launch(galleryIntent);
+            }
+        });
+
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, 110);
+            }
+
+        });
+
+
+        uploadBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (imageUri == null){
+                    Toast.makeText(getContext(), "No Image Selected", Toast.LENGTH_LONG).show();
+                }else {
+                    uploadPicture();
+                }
+            }
+        });
+    }
+
+
+    ActivityResultLauncher<Intent> galleryActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result){
+                    if(result.getResultCode() == getActivity().RESULT_OK) {
+                        imageUri = result.getData().getData();
+                        imageView.setImageURI(imageUri);
+                    }
+                }
+            });
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 110){
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(captureImage);
+            imageUri = getImageUri(getContext(), captureImage);
+        }
+    }
+
+
+    private void uploadPicture(){
+
+        final ProgressDialog pd = new ProgressDialog(getContext());
+        pd.setTitle("Uploading Image...");
+        pd.show();
+
+        final String randomKey = UUID.randomUUID().toString();
+        StorageReference fileRef = storageReference.child("images/" + randomKey);
+
+        fileRef.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(getContext(), "Uploaded Successfully!", Toast.LENGTH_LONG).show();
+                        pd.dismiss();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getContext(), "Failed to Upload!", Toast.LENGTH_LONG).show();
+                        pd.dismiss();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                        double progressPercent = (100 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                        pd.setMessage("Percentage: " + (int) progressPercent + "%");
+                    }
+                });
+    };
+
+
+    private Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
 }
