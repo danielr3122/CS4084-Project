dd40af066b1b3d7c0ca559fad39b499434e5ab4a
danielr3122 <drosenhamer@gmail.com>
2023-04-18 22:38:27 +0100

    Removed unused imports

--- app/src/main/java/com/example/cs4084project/PostsAdapter.java
+++ app/src/main/java/com/example/cs4084project/PostsAdapter.java
@@ -2,22 +2,18 @@ package com.example.cs4084project;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-
-import kotlin.internal.UProgressionUtilKt;
 
 public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
 
