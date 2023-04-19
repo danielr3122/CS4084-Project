28741f1aea144152d688a6a242b7791e1393ff91
danielr3122 <drosenhamer@gmail.com>
2023-04-19 12:13:22 +0100

    Removed unused code

--- app/src/main/java/com/example/cs4084project/SettingsFragment.java
+++ app/src/main/java/com/example/cs4084project/SettingsFragment.java
@@ -1,8 +1,6 @@
 package com.example.cs4084project;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -15,14 +13,9 @@ import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.Objects;
-import java.util.zip.Inflater;
-
 
 public class SettingsFragment extends Fragment {
 
-Button buttonLogout;
-FirebaseAuth mAuth, auth;
     public SettingsFragment() {
         // Required empty public constructor
     }
@@ -34,23 +27,17 @@ FirebaseAuth mAuth, auth;
         View view = inflater.inflate(R.layout. fragment_settings, container, false);
 
         Button buttonLogout = (Button) view.findViewById(R.id.btn_Logout);
-        buttonLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getActivity(), Login.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-                requireActivity().finish();
-            }
+        buttonLogout.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(getActivity(), Login.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            requireActivity().finish();
         });
 
         Button buttonChangePassword = view.findViewById(R.id.btn_change_pw);
-        buttonChangePassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-            }
+        buttonChangePassword.setOnClickListener(view1 -> {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         });
 
         return view;
